0a9c62a36d5f96210a4e3f15a2f7355a3e79ddad
tonyjose756 <jost1_16@uni.worc.ac.uk>
2017-11-13 12:12:55 +0000

    change
    d

--- www/js/index.js
+++ www/js/index.js
@@ -8,13 +8,28 @@ $(document).on("pagecreate","#pageone",function(){
 
 function submitText() {
     
-     var fred = random();
+     var trueorfalse = random();
+       
+	$('#textinput').text(trueorfalse);  //javascript function
     
+    if (trueorfalse)
+        {
+            navigator.notification.beep(1);
+            //alert("true");
+        }
+    else
+        {
+            navigator.notification.beep(2);
+            //alert("false");
+        }
     
-	$('#textinput').text(fred);
 }
+    
 
 function random() {
-	return !Math.round(Math.random());
+	return !Math.round(Math.random()); //generates random true or false  !converts 1 and 0 to true or false
 }
+
+
+
 
