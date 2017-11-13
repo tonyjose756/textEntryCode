35ae2dc89e909bf4c6dc40183703feeda58d0e29
tonyjose756 <jost1_16@uni.worc.ac.uk>
2017-11-13 11:00:25 +0000

    add plugins

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,20 @@
 $(document).on("pagecreate","#pageone",function(){
+    
   $('#submitButton').on("click", function(){
     submitText();
   });            
-});            
+});         
 
 
 function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
+    
+     var fred = random();
+    
+    
+	$('#textinput').text(fred);
 }
 
-function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
+function random() {
+	return !Math.round(Math.random());
 }
+
